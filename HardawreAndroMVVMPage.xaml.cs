b7989b877b3f2e6b21e5e4f53a0ede2fe5bfced9
Veli Bacık <tercih2017bsm@gmail.com>
2018-01-02 23:10:47 +0300

    MVVM 1
    Implements INotifyPropertyChanged

---
+++ HardawreAndroMVVMPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using HardawreAndroMVVM.User;
+using Xamarin.Forms;
+
+namespace HardawreAndroMVVM
+{
+    public partial class HardawreAndroMVVMPage : ContentPage
+    {
+        public HardawreAndroMVVMPage()
+        {
+            InitializeComponent();
+
+
+            BindingContext = new Userx();
+          
+        }
+    }
+}
---
+++ User/Userx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HardawreAndroMVVM.User
+{
+    public class Userx : INotifyPropertyChanged
+    {
+        string UserName;
+
+        public string userName
+        {
+            get
+            {
+                return UserName;
+            }
+
+            set
+            {
+                UserName = value;
+                OnPropertyChanged("userName");
+                OnPropertyChanged("fullName");
+            }
+        }
+        string UserSurname;
+
+
+        public string userSurname
+        {
+            get
+            {
+                return UserSurname;
+            }
+
+            set
+            {
+                UserSurname = value;
+                OnPropertyChanged("userSurname");
+                OnPropertyChanged("fullName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string fullName
+        {
+            get {
+                return userName + " : " + userSurname;
+            }
+        }
+
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+    }
+}
